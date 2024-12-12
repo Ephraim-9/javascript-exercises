76d885e825b0922315160850d3a4fcebe7bc1427
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2024-12-12 19:44:46 +0500

    03_reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var arr = Array.from(str)
+    var revarr = []
+    
+    for (var i = arr.length - 1; i >= 0; i--) {
+        revarr.push(arr[i])
+    }
+    var revstr = revarr.join("")
+    return(revstr)
 };
-
 // Do not edit below this line
 module.exports = reverseString;
