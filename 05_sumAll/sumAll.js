139294074d9e9eba7e3264f5419fe63f7f35f3fa
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2024-12-19 19:58:00 +0500

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let loopAdd = function(c,d) {
+        let i = c 
+        do {
+             i++;
+             c += i;
+        } while (i < d)
+            return c;
+    }
+    if (a > 0 && b > 0 ) {
+        if (!Number.isInteger(a) || !Number.isInteger(b)) {return "ERROR"}
+        else if (a < b) {
+            let sum = loopAdd(a,b)
+            return sum;
+        }
+        else {
+            let sum = loopAdd(b,a)
+            return sum;
+        }
+    }
+    else {return "ERROR"}
 };
+let added = sumAll(1.4, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+console.log(added)
 
 // Do not edit below this line
 module.exports = sumAll;
