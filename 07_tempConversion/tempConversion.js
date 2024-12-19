955fe47c976539c27df08904e7aa9656e0b574a4
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2024-12-19 21:05:22 +0500

    exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let converted = (f-32)* 5/9
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let converted = c*9/5 + 32
+  return Math.round(converted * 10) / 10;
 };
+convertToCelsius(100) // fahrenheit to celsius, should return 0
+
+convertToFahrenheit(1) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
