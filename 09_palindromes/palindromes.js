0ea8f7cf35d3efd54f91bdb7cee59635aa9f38c6
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2024-12-20 12:51:26 +0500

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+let normal = function (words) {
+    return words
+        .map(str => 
+            str.replace(/[^\w]/g, "") // Remove all non-word characters (punctuation and spaces)
+               .toLowerCase()          // Convert to lowercase
+        )
+        .filter(str => str !== ""); // Filter out any empty strings
+}
 
+const palindromes = function (word) {
+    let wordarr = word.split("");
+    const arr = normal(wordarr);
+    let revarr = arr.slice().reverse();
+    return revarr.every((value, index) => value === arr[index]);
 };
+palindromes('racecar!') 
+//npm test palindromes.spec.
 
 // Do not edit below this line
 module.exports = palindromes;
